1aabebca4a73400d49303d0fead5de51b8dc187f
Mark W. Ogren <mwogren@hotmail.com>
2019-11-26 10:17:14 -0500

    Add files via upload

---
+++ glove_oc_sketch_July2015.ino
@@ -0,0 +1,88 @@
+void setup(){
+
+pinMode(5,INPUT_PULLUP);
+pinMode(4,INPUT_PULLUP);
+pinMode(3,INPUT_PULLUP);
+pinMode(2,INPUT_PULLUP);
+
+Keyboard.begin();
+
+}
+
+void loop(){
+
+if(digitalRead(5) == LOW){
+
+Keyboard.press(KEY_LEFT_CTRL);
+
+Keyboard.press(KEY_LEFT_ALT);
+
+Keyboard.press('3');
+
+delay(50);
+
+Keyboard.releaseAll();
+
+delay(500);
+
+}
+
+
+{
+
+if(digitalRead(4) == LOW){
+
+Keyboard.press(KEY_LEFT_GUI);
+
+Keyboard.press(KEY_RIGHT_ARROW);
+
+delay(50);
+
+Keyboard.releaseAll();
+
+delay(500);
+
+}
+
+{
+
+if(digitalRead(3) == LOW){
+
+Mouse.press(MOUSE_LEFT );
+
+delay(100);
+
+while (digitalRead(3) == LOW){
+
+delay(10);
+
+}
+
+Mouse.release();
+
+}
+}
+
+
+{
+
+if(digitalRead(2) == LOW){
+
+Mouse.press(MOUSE_RIGHT);
+
+delay(100);
+
+while (digitalRead(2) == LOW){
+
+delay(10);
+
+}
+
+Mouse.release();
+
+}
+}
+
+
+}
+}
